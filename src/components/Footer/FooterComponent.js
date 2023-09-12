605691e1de1f47f9ece68e55214cc3c739dc7ea5
Kamran Khan <kamrankhan.sde@gmail.com>
2023-09-13 00:02:47 +0530

    Footer Updated

--- src/components/Footer/FooterComponent.js
+++ src/components/Footer/FooterComponent.js
@@ -35,14 +35,90 @@ function FooterComponent() {
               />
             </Col>
           </Row>
-          <Row>
+          <Row className="pt-4">
             <Col lg={3} className="text-center">
+              <h4>Company</h4>
+              <p>lorem</p>
             </Col>
-            <Col lg={3} className="text-center">
+            <Col lg={3} className="for-xplorers">
+              <h4>For Xplorers</h4>
+              <ul>
+                <li>
+                  <a>Explore Navi Mumbai</a>
+                </li>
+                <li>
+                  <a>Navi Mumbai Travel Hacks</a>
+                </li>
+                <li>
+                  <a>Live Announcements</a>
+                </li>
+                <li>
+                  <a>Commuter News</a>
+                </li>
+                <li>
+                  <a>Media Coverage</a>
+                </li>
+              </ul>
             </Col>
-            <Col lg={3} className="text-center">
+            <Col lg={3} className="quick-links">
+              <h4>Quick Links</h4>
+              <ul>
+                <li>
+                  <a>Home</a>
+                </li>
+                <li>
+                  <a>About NaviXplore App</a>
+                </li>
+                <li>
+                  <a>Blog</a>
+                </li>
+                <li>
+                  <a>Advertise with us</a>
+                </li>
+                <li>
+                  <a>Terms Of Use</a>
+                </li>
+                <li>
+                  <a>Privacy Policy</a>
+                </li>
+              </ul>
             </Col>
-            <Col lg={3} className="text-center">
+            <Col lg={3} className="follow-us">
+              <h4 className="text-center">Follow Us</h4>
+              <ul className="social-icons">
+                <li>
+                  <a>
+                    <i className="bi bi-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <i className="bi bi-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <i className="bi bi-instagram"></i>
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <i className="bi bi-linkedin"></i>
+                  </a>
+                </li>
+                <li>
+                  <a>
+                    <i className="bi bi-youtube"></i>
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+          <Row className="pt-4 pb-2">
+            <Col lg={12} className="text-center">
+              <span className="footer-description">
+                © 2023 NaviXplore. All rights reserved.
+              </span>
             </Col>
           </Row>
         </Container>

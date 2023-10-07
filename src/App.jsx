f3aa60032aea5cfe74fc7d0c78b91dfccc45a316
Kamran Khan <kamrankhan.sde@gmail.com>
2023-10-08 00:26:21 +0530

    Page Routing Changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./scss/App.scss";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState, useEffect } from "react";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.css";
@@ -14,7 +14,6 @@ import MediaCoverage from "./pages/MediaCoverage";
 import AdvertiseWithUs from "./pages/AdvertiseWithUs";
 import NaviXploreAppFeatures from "./pages/NaviXploreAppFeatures";
 
-
 import Navbar from "./components/Navbar/NavbarComponent";
 import Footer from "./components/Footer/FooterComponent";
 import PostPage from "./components/[PostPage]";
@@ -22,54 +21,59 @@ import PostPage from "./components/[PostPage]";
 import FamousPlacesJSON from "./json/FamousPlaces.json";
 import TouristDestinationsJSON from "./json/TouristDestinations.json";
 
-
 function App() {
   const [FamousPlaces, setFamousPlaces] = useState([]);
   const [TouristDestinations, setTouristDestinations] = useState([]);
 
   useEffect(() => {
     setFamousPlaces(FamousPlacesJSON);
-}, []);
+  }, []);
 
-useEffect(() => {
+  useEffect(() => {
     setTouristDestinations(TouristDestinationsJSON);
-}, []);
-
+  }, []);
 
   return (
     <>
-    <Navbar />
-    <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/changelogs" element={<Changelogs />} />
-        <Route path="/feedback" element={<Feedback />} />
-        <Route path="/explore-navi-mumbai" element={< ExploreNaviMumbai/>} />
-        <Route path="/navi-mumbai-metro-map" element={<NaviMumbaiMetroMap />} />
-        <Route path="/mumbai-local-railmap" element={<MumbaiLocalRailMap />} />
-        <Route path="/navixplore-app-features" element={<NaviXploreAppFeatures/>} />
-        <Route path="/media-coverage" element={<MediaCoverage />} />
-        <Route path="/advertise-with-us" element={<AdvertiseWithUs />} />
-        {
-          FamousPlaces.map(item => (
-              <Route
-                key={item.id}
-                path={`/places/${item.name}`}
-                element={<PostPage data={item}/>}
-              />
-          ))
-        }
-        {
-          TouristDestinations.map(item => (
-              <Route
-                key={item.id}
-                path={`/places/${item.name}`}
-                element={<PostPage data={item}/>}
-              />
-          ))
-        }
-      </Routes>
-      <Footer />
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/changelogs" element={<Changelogs />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/explore-navi-mumbai" element={<ExploreNaviMumbai />} />
+          <Route
+            path="/navi-mumbai-metro-map"
+            element={<NaviMumbaiMetroMap />}
+          />
+          <Route
+            path="/mumbai-local-railmap"
+            element={<MumbaiLocalRailMap />}
+          />
+          <Route
+            path="/navixplore-app-features"
+            element={<NaviXploreAppFeatures />}
+          />
+          <Route path="/media-coverage" element={<MediaCoverage />} />
+          <Route path="/advertise-with-us" element={<AdvertiseWithUs />} />
+          {FamousPlaces.map((item) => (
+            <Route
+              key={item.id}
+              path={`/places/${item.name}`}
+              element={<PostPage data={item} />}
+            />
+          ))}
+          {TouristDestinations.map((item) => (
+            <Route
+              key={item.id}
+              path={`/places/${item.name}`}
+              element={<PostPage data={item} />}
+            />
+          ))}
+        </Routes>
+        <Footer />
+      </Router>
     </>
   );
 }

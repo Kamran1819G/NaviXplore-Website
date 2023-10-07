206157e9502719f27dfa89d8c06e93ad95231d5f
Kamran Khan <kamrankhan.sde@gmail.com>
2023-10-08 00:40:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -39,35 +39,35 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/changelogs" element={<Changelogs />} />
-          <Route path="/feedback" element={<Feedback />} />
-          <Route path="/explore-navi-mumbai" element={<ExploreNaviMumbai />} />
+          <Route path="home" element={<Home />} />
+          <Route path="changelogs" element={<Changelogs />} />
+          <Route path="feedback" element={<Feedback />} />
+          <Route path="explore-navi-mumbai" element={<ExploreNaviMumbai />} />
           <Route
-            path="/navi-mumbai-metro-map"
+            path="navi-mumbai-metro-map"
             element={<NaviMumbaiMetroMap />}
           />
           <Route
-            path="/mumbai-local-railmap"
+            path="mumbai-local-railmap"
             element={<MumbaiLocalRailMap />}
           />
           <Route
-            path="/navixplore-app-features"
+            path="navixplore-app-features"
             element={<NaviXploreAppFeatures />}
           />
-          <Route path="/media-coverage" element={<MediaCoverage />} />
-          <Route path="/advertise-with-us" element={<AdvertiseWithUs />} />
+          <Route path="media-coverage" element={<MediaCoverage />} />
+          <Route path="advertise-with-us" element={<AdvertiseWithUs />} />
           {FamousPlaces.map((item) => (
             <Route
               key={item.id}
-              path={`/places/${item.name}`}
+              path={`places/${item.name}`}
               element={<PostPage data={item} />}
             />
           ))}
           {TouristDestinations.map((item) => (
             <Route
               key={item.id}
-              path={`/places/${item.name}`}
+              path={`places/${item.name}`}
               element={<PostPage data={item} />}
             />
           ))}

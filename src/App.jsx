1b42f233be84598cd9362d75df9688c28914c290
Kamran Khan <kamrankhan.sde@gmail.com>
2023-10-08 01:23:49 +0530

    Added ReWriteAll plugin

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import NaviMumbaiMetroMap from "./pages/NaviMumbaiMetroMap";
 import MediaCoverage from "./pages/MediaCoverage";
 import AdvertiseWithUs from "./pages/AdvertiseWithUs";
 import NaviXploreAppFeatures from "./pages/NaviXploreAppFeatures";
+import NotFound from "./pages/NotFound";
 
 import Navbar from "./components/Navbar/NavbarComponent";
 import Footer from "./components/Footer/FooterComponent";
@@ -39,38 +40,38 @@ function App() {
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route path="home" element={<Home />} />
-          <Route path="changelogs" element={<Changelogs />} />
-          <Route path="feedback" element={<Feedback />} />
-          <Route path="explore-navi-mumbai" element={<ExploreNaviMumbai />} />
+          <Route path="/changelogs" element={<Changelogs />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/explore-navi-mumbai" element={<ExploreNaviMumbai />} />
           <Route
-            path="navi-mumbai-metro-map"
+            path="/navi-mumbai-metro-map"
             element={<NaviMumbaiMetroMap />}
           />
           <Route
-            path="mumbai-local-railmap"
+            path="/mumbai-local-railmap"
             element={<MumbaiLocalRailMap />}
           />
           <Route
-            path="navixplore-app-features"
+            path="/navixplore-app-features"
             element={<NaviXploreAppFeatures />}
           />
-          <Route path="media-coverage" element={<MediaCoverage />} />
-          <Route path="advertise-with-us" element={<AdvertiseWithUs />} />
+          <Route path="/media-coverage" element={<MediaCoverage />} />
+          <Route path="/advertise-with-us" element={<AdvertiseWithUs />} />
           {FamousPlaces.map((item) => (
             <Route
               key={item.id}
-              path={`places/${item.name}`}
+              path={`/places/${item.name}`}
               element={<PostPage data={item} />}
             />
           ))}
           {TouristDestinations.map((item) => (
             <Route
               key={item.id}
-              path={`places/${item.name}`}
+              path={`/places/${item.name}`}
               element={<PostPage data={item} />}
             />
           ))}
+          <Route path="*" element={<NotFound/>} />
         </Routes>
         <Footer />
       </Router>
---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,15 @@
+import { Container, Row } from "react-bootstrap";
+
+function Notfound() {
+  return (
+    <>
+      <Container>
+        <Row className="justify-content-center">
+          <h1>404 Not Found</h1>
+        </Row>
+      </Container>
+    </>
+  );
+}
+
+export default Notfound;
